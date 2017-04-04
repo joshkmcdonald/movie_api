992694f6898c7236c661b5280056631398d249d1
joshkmcdonald <joshkmcdonald@gmail.com>
2017-04-04 13:39:23 -0600

    Generate Pages Controller. Build out form_tag in search view, connect to movie index.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Tmdb::Search.movie('Moana').results
+    @movies = Tmdb::Search.movie(params[:search]).results
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/search'
+
+  get 'home/search'
+
   get 'movies/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'movies#index'
 end
