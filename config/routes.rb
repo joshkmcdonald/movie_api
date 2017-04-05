eeb1fa047e0497059b66f0245ccb2ce3e0852d76
joshkmcdonald <joshkmcdonald@gmail.com>
2017-04-04 23:40:40 -0600

    Add basic shared application nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   get 'pages/search'
 
-  get 'home/search'
-
   get 'movies/index'
 
-  root to: 'movies#index'
+  root to: 'pages#home'
 end
